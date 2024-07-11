243f61d2d22c4727c990b47029e0da27cbfe6da7
Paula Lavalle <gitlab.rl66l@passmail.net>
2024-07-11 14:30:13 -0700

    Add room boundaries and constrain moveable point within room

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,14 +1,56 @@
-import { Mafs, Coordinates, Debug } from 'mafs';
+import { Mafs, Coordinates, Debug, Point, Polygon, useMovablePoint, Theme, Line } from 'mafs';
 import * as React from 'react';
 
+const ROOM_WIDTH = 5;
+const ROOM_HEIGHT = 3;
+
 export default function App() {
+  const c = useMovablePoint([3, 0], {
+    // Constrain `point` to be within the boundaries of the room
+    constrain: ([x, y]) => {
+      const withinRoom = (a: number, max: number) => {
+        if (a > max) {
+          return max;
+        } else if (a < 0) {
+          return 0;
+        } else {
+          return a;
+        }
+      };
+
+      return [withinRoom(x, ROOM_WIDTH), withinRoom(y, ROOM_HEIGHT)];
+    },
+  });
+
   return (
-    <Mafs viewBox={{ y: [-1, 1], x: [-1, 1] }}>
+    <Mafs viewBox={{ y: [0, ROOM_HEIGHT], x: [-ROOM_WIDTH, ROOM_WIDTH * 5] }}>
       <Coordinates.Cartesian />
 
-      <Debug.TransformWidget>
-        <PizzaSlice />
-      </Debug.TransformWidget>
+      {/* <Point x={c.x} y={c.y} color={Theme.blue} /> */}
+
+      <Line.Segment point1={[0, ROOM_HEIGHT]} point2={[ROOM_WIDTH, ROOM_HEIGHT]} />
+      <Line.Segment point1={[0, 0]} point2={[0, ROOM_HEIGHT]} color={Theme.blue} />
+      <Line.Segment point1={[ROOM_WIDTH, ROOM_HEIGHT]} point2={[ROOM_WIDTH, 0]} color={Theme.blue} />
+      <Line.Segment point1={[0, 0]} point2={[ROOM_WIDTH, 0]} />
+
+      <Point x={-c.x} y={c.y} />
+      <Point x={c.x + ROOM_WIDTH} y={c.y} />
+
+      {/* <Polygon points={[[-c.x, c.y], a, b]} strokeStyle="dashed" />
+      <Polygon
+        points={[
+          [c.x + 5, c.y],
+          [a[0] + 5, a[1]],
+          [b[0] + 5, b[1]],
+        ]}
+        strokeStyle="dashed"
+      /> */}
+      {/* <Polygon points={[c.point, a, b]} color={Theme.blue} /> */}
+      {c.element}
+
+      {/* <Debug.TransformWidget> */}
+      {/* <PizzaSlice /> */}
+      {/* </Debug.TransformWidget> */}
     </Mafs>
   );
 }
