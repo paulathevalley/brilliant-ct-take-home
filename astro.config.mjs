cc798218eb60843aa1b22288461bc14802d9f2a4
Paula Lavalle <gitlab.rl66l@passmail.net>
2024-07-12 16:38:55 -0700

    Successfully add custom eye svg

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,21 @@
 import { defineConfig } from 'astro/config';
+// astro integrations
 import react from '@astrojs/react';
+// vite plugins
+import svgr from 'vite-plugin-svgr';
 
 // https://astro.build/config
 export default defineConfig({
   integrations: [react()],
   vite: {
     ssr: { noExternal: ['mafs'] },
+    plugins: [
+      svgr({
+        svgrOptions: {
+          // https://react-svgr.com/docs/options/#ref
+          ref: true,
+        },
+      }),
+    ],
   },
 });
--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,8 +1,27 @@
-import { Mafs, Coordinates, Debug, Point, Polygon, useMovablePoint, Theme, Line } from 'mafs';
 import * as React from 'react';
+import { Mafs, Coordinates, Debug, Point, Polygon, useMovablePoint, Theme, Line } from 'mafs';
+import EYE from '../assets/eye.svg';
+import RawEyeSvg from '../assets/eye.svg?raw';
 
 const ROOM_WIDTH = 5;
 const ROOM_HEIGHT = 3;
+
+console.log(EYE);
+console.log(RawEyeSvg);
+
+function EyeAsset() {
+  return (
+    <>
+      <g style={{ transform: `var(--mafs-view-transform) var(--mafs-user-transform)` }}>
+        <clipPath id="myspecialclip">
+          <path d="M.534 0c.024 0 .043.02.043.044a.167.167 0 0 1-.025.08.5.5 0 0 1-.057.077 1.562 1.562 0 0 1-.161.155A3.415 3.415 0 0 1 .153.5a3.276 3.276 0 0 1 .18.139C.392.686.452.74.497.793a.478.478 0 0 1 .056.08.18.18 0 0 1 .025.083.044.044 0 1 1-.087 0A.095.095 0 0 0 .476.914.393.393 0 0 0 .429.85 1.318 1.318 0 0 0 .278.705 2.896 2.896 0 0 0 .054.537L0 .5.053.464a1.232 1.232 0 0 0 .07-.051C.164.381.22.338.276.29.334.242.39.19.43.144A.414.414 0 0 0 .476.081.085.085 0 0 0 .49.044C.49.02.51 0 .534 0Z" />
+          <path d="M.63.247a.288.288 0 0 0 0 .508.456.456 0 0 0 0-.508Z" />
+        </clipPath>
+        <rect clip-path="url(#myspecialclip)" width="2" height="2" fill="#000" />
+      </g>
+    </>
+  );
+}
 
 export default function App() {
   const c = useMovablePoint([3, 0], {
@@ -26,8 +45,6 @@ export default function App() {
     <Mafs viewBox={{ y: [0, ROOM_HEIGHT], x: [-ROOM_WIDTH, ROOM_WIDTH * 5] }}>
       <Coordinates.Cartesian />
 
-      {/* <Point x={c.x} y={c.y} color={Theme.blue} /> */}
-
       <Line.Segment point1={[0, ROOM_HEIGHT]} point2={[ROOM_WIDTH, ROOM_HEIGHT]} />
       <Line.Segment point1={[0, 0]} point2={[0, ROOM_HEIGHT]} color={Theme.blue} />
       <Line.Segment point1={[ROOM_WIDTH, ROOM_HEIGHT]} point2={[ROOM_WIDTH, 0]} color={Theme.blue} />
@@ -36,17 +53,9 @@ export default function App() {
       <Point x={-c.x} y={c.y} />
       <Point x={c.x + ROOM_WIDTH} y={c.y} />
 
-      {/* <Polygon points={[[-c.x, c.y], a, b]} strokeStyle="dashed" />
-      <Polygon
-        points={[
-          [c.x + 5, c.y],
-          [a[0] + 5, a[1]],
-          [b[0] + 5, b[1]],
-        ]}
-        strokeStyle="dashed"
-      /> */}
-      {/* <Polygon points={[c.point, a, b]} color={Theme.blue} /> */}
       {c.element}
+
+      <EyeAsset />
 
       {/* <Debug.TransformWidget> */}
       {/* <PizzaSlice /> */}
